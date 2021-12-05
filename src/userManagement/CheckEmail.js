225415c3fb7f6937666d336f54bb1745597a5776
Marko Milanovic <marko.milanovic@travelperk.com>
2021-12-06 00:53:07 +0100

    Pass reset fix

--- src/userManagement/CheckEmail.js
+++ src/userManagement/CheckEmail.js
@@ -7,7 +7,8 @@ export default function CheckYourEmail(){
     };
     return (
         <span className="auth__question">
-            Password reset initiated successfully, check you email (don't forget the spam folder!)
+            Password reset initiated successfully, check your email (don't forget the spam folder!)
+            <br/>
             <button
                 className="btn btn--primary"
                 onClick={async () => await redirectToLogin()}
